f684fd6f97b33be9b8c595b1c60ce9680d6207ff
Peter Huber <49078578+PeterHuberSg@users.noreply.github.com>
2022-08-24 19:05:52 +0800

    Add project files.

---
+++ LogViewerLib/LogViewer.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace LogViewerLib {
+
+
+  /// <summary>
+  /// This WPF control displays logging information, scrolling the displayed text if there is not enough screen space. 
+  /// The text can be bold, italic, red, etc.
+  /// Some information can be written temporarly. This kind of text gets only written to the screen when nothing else 
+  /// was written for 300 msecs and it gets overwritten by the next log text.
+  /// </summary>
+  public class LogViewer: RichTextBox {
+
+    #region Properties
+    //      ----------
+
+    /// <summary>
+    /// How long a temporary write gets delayed before it is shown to the user
+    /// </summary>
+    public int TemporaryDelay {
+      get { return (int)GetValue(TemporaryDelayProperty); }
+      set { SetValue(TemporaryDelayProperty, value); }
+    }
+
+    // DependencyProperty definition
+    public static readonly DependencyProperty TemporaryDelayProperty =
+        DependencyProperty.Register("TemporaryDelay", typeof(int), typeof(LogViewer), new PropertyMetadata(300));
+    #endregion
+
+
+    #region Constructor
+    //      -----------
+
+    readonly FlowDocument flowDoc;
+    Paragraph styledParagraph;
+
+
+    public LogViewer() {
+      HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+      VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+      AcceptsReturn = false;
+      IsReadOnly = true;
+      IsUndoEnabled = false;
+
+      flowDoc = new FlowDocument();
+      Document = flowDoc;
+      styledParagraph = getEmptyParagraph();
+    }
+    #endregion
+
+
+    #region Public Methods
+    //      --------------
+
+    /// <summary>
+    /// Empties the LogViwer content
+    /// </summary>
+    public void Clear() {
+      flowDoc.Blocks.Clear();
+    }
+
+
+    public void WriteLine() {
+      invokeWrite(new StyledString("", StringStyleEnum.normal, LineHandlingEnum.endOfLine));
+    }
+
+
+    public void WriteLine(string line) {
+      invokeWrite(new StyledString(line, LineHandlingEnum.endOfLine));
+    }
+
+
+    /// <summary>
+    /// This line gets only shown to the user when nothing else gets written for a short while and it will get overwritten
+    /// by the next WriteXxx(). This is usefull when the user should get only few notifications per second even there
+    /// might be many WriteTempXxx() be executed.
+    /// </summary>
+    public void WriteTempLine(string line) {
+      invokeWrite(new StyledString(line, LineHandlingEnum.temporaryEOL));
+    }
+
+
+    public void WriteLine(string line, StringStyleEnum stringStyle) {
+      invokeWrite(new StyledString(line, stringStyle, LineHandlingEnum.endOfLine));
+    }
+
+
+    /// <summary>
+    /// This line gets only shown to the user when nothing else gets written for a short while and it will get overwritten
+    /// by the next WriteXxx(). This is usefull when the user should get only few notifications per second even there
+    /// might be many WriteTempXxx() be executed.
+    /// </summary>
+    public void WriteTempLine(string line, StringStyleEnum stringStyle) {
+      invokeWrite(new StyledString(line, stringStyle, LineHandlingEnum.endOfLine));
+    }
+
+
+    public void Write(string text) {
+      invokeWrite(new StyledString(text));
+    }
+
+
+    public void Write(string text, StringStyleEnum stringStyle) {
+      invokeWrite(new StyledString(text, stringStyle));
+    }
+
+
+    public void Write(StyledString styledString) {
+      invokeWrite(styledString);
+    }
+
+
+    public void Write(params StyledString[] styledStrings) {
+      invokeWrite(styledStrings);
+    }
+    #endregion
+
+
+    #region Private methods
+    //      ---------------
+
+    private void invokeWrite(object styledObject) {
+      if (!CheckAccess()) {
+        //we are on a different thread, synchronise with the WPF Window thread.
+        this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background,
+         new Action<object>(invokeWrite), styledObject);
+      }
+
+      if (styledObject is StyledString styledString) {
+        append(styledString);
+      } else if (styledObject is StyledString[] styledStrings) {
+        foreach (StyledString styledString2 in styledStrings) {
+          append(styledString2);
+        }
+      } else {
+        throw new NotSupportedException($"LogViewer.Write(): unsupported type '{styledObject.GetType()}' with content '{styledObject}'.");
+      }
+      ScrollToEnd();
+    }
+
+
+    bool lastLineWasTemporary;
+
+
+    private void append(StyledString styledString) {
+      //breakup styledString into lines
+      String[] lineStrings = styledString.String.Split(Environment.NewLine, StringSplitOptions.None);
+      for (int lineIndex = 0; lineIndex < lineStrings.Length; lineIndex++) {
+
+        //Process every line
+        string lineString = lineStrings[lineIndex];
+        Inline inline;
+
+        //apply styles
+        switch (styledString.StringStyle) {
+        case StringStyleEnum.errorHeader:
+          styledParagraph.Margin = new Thickness(0, 24, 0, 4);
+          inline = new Bold(new Run(lineString));
+          inline.FontSize = styledParagraph.FontSize * 1.2;
+          inline.Foreground = Brushes.Red;
+          break;
+        case StringStyleEnum.errorText:
+          inline = new Run(lineString);
+          inline.Foreground = Brushes.Red;
+          break;
+        case StringStyleEnum.label:
+          inline = new Run(lineString);
+          inline.Foreground = Brushes.MidnightBlue;
+          break;
+        case StringStyleEnum.header1:
+          styledParagraph.Margin = new Thickness(0, 24, 0, 4);
+          inline = new Bold(new Run(lineString));
+          inline.FontSize = styledParagraph.FontSize * 1.2;
+          break;
+        case StringStyleEnum.normal:
+        case StringStyleEnum.none:
+        default:
+          inline = new Run(lineString);
+          break;
+        }
+
+        if (lastLineWasTemporary) {
+          //last line was temporary. overwrite it
+          lastLineWasTemporary = false;
+          styledParagraph.Inlines.Clear();
+        }
+
+        //if last styledString was end of line, start a new paragraph to flow document.
+        if (styledParagraph.Inlines.Count==0) {
+          flowDoc.Blocks.Add(styledParagraph);
+          if (styledString.LineHandling!=LineHandlingEnum.endOfLine || styledString.String.Length>0) {
+            //not an empty line. Add the time
+            Run timeRun = new(styledString.Created.ToString("hh:mm:ss  ")) {
+              Foreground = Brushes.DimGray
+            };
+            styledParagraph.Inlines.Add(timeRun);
+          }
+        }
+
+        //add line
+        styledParagraph.Inlines.Add(inline);
+
+        //check for end of line
+        if (styledString.LineHandling==LineHandlingEnum.endOfLine) {
+          styledParagraph = getEmptyParagraph();
+        } else if (styledString.LineHandling==LineHandlingEnum.temporaryEOL) {
+          lastLineWasTemporary = true;
+        } else if (lineIndex < lineStrings.Length-1) {
+          styledParagraph = getEmptyParagraph();
+        }
+      }
+    }
+
+
+    private Paragraph getEmptyParagraph() {
+      Paragraph newParagraph = new Paragraph();
+      newParagraph.Margin = new Thickness(0);
+      return newParagraph;
+    }
+
+
+    //public void DoEvents() {
+    //  DispatcherFrame frame = new DispatcherFrame();
+    //  Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background,
+    //      new DispatcherOperationCallback(ExitFrames), frame);
+    //  Dispatcher.PushFrame(frame);
+    //}
+
+    //public object ExitFrames(object f) {
+    //  ((DispatcherFrame)f).Continue = false;
+
+    //  return null;
+    //}
+    #endregion
+
+  }
+}
---
+++ LogViewerLib/StyledString.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+
+namespace LogViewerLib {
+
+  #region Delegates
+  //      ---------
+
+  /// <summary>
+  /// Encapsulates a method that takes one or many StyledString as parameter and does not return a value.
+  /// </summary>
+  /// <param name="s"></param>
+  public delegate void ActionStyledStringDelegate(params StyledString[] styledString);
+  #endregion
+
+
+  #region Enumerations
+  //      ------------
+
+  /// <summary>
+  /// StringStyleEnum can be applied to StyledString. Works similarly like styles in HTML or Word.
+  /// </summary>
+  public enum StringStyleEnum {
+    none = 0,
+    normal,
+    label,
+    header1,
+    errorHeader,
+    errorText,
+  }
+
+
+  /// <summary>
+  /// Does the StyledString mark the end of a line ? If temporary, the next line will overwrite the current line
+  /// </summary>
+  public enum LineHandlingEnum {
+    none = 0,
+    endOfLine,
+    temporaryEOL,
+  }
+  #endregion
+
+
+  /// <summary>
+  /// A class to store a string together with some formatting instructions. The formatting is technology
+  /// independent and can be used for WPF, HTML, etc.
+  /// </summary>
+  public class StyledString {
+    #region Properties
+    //      ----------
+
+    /// <summary>
+    /// actual string
+    /// </summary>
+    public string String { get; private set; }
+
+    /// <summary>
+    /// Style to be applied to string
+    /// </summary>
+    public StringStyleEnum StringStyle { get; private set; }
+
+    /// <summary>
+    /// Should a new line be added after this string ? A string can actually contain
+    /// Environment.NewLine, meaning one StyledString might cove several lines. LineHandling
+    /// applies to the end of StyledString.
+    /// </summary>
+    public LineHandlingEnum LineHandling { get; private set; }
+
+    /// <summary>
+    /// time when string was created. Automatically filled in.
+    /// </summary>
+    public DateTime Created { get; private set; }
+    #endregion
+
+
+    #region Constructor
+    //      -----------
+    /// <summary>
+    /// constructor
+    /// </summary>
+    public StyledString(string newString, StringStyleEnum newStringStyle, LineHandlingEnum newLineHandling) {
+      String = newString;
+      StringStyle = newStringStyle;
+      LineHandling = newLineHandling;
+      Created = DateTime.Now;
+    }
+
+
+    /// <summary>
+    /// constructor, style is normal
+    /// </summary>
+    public StyledString(string newStringString, LineHandlingEnum newLineHandling) :
+      this(newStringString, StringStyleEnum.normal, newLineHandling) { }
+
+
+    /// <summary>
+    /// constructor, no new line needed
+    /// </summary>
+    public StyledString(string newStringString, StringStyleEnum newStringStyle) :
+      this(newStringString, newStringStyle, LineHandlingEnum.none) { }
+
+
+    /// <summary>
+    /// constructor, style is normal, no new line needed
+    /// </summary>
+    public StyledString(string newStringString) :
+      this(newStringString, StringStyleEnum.normal, LineHandlingEnum.none) { }
+
+
+    /// <summary>
+    /// constructor, probably for empty new line
+    /// </summary>
+    public StyledString(LineHandlingEnum newLineHandling) :
+      this("", StringStyleEnum.normal, newLineHandling) { }
+    #endregion
+
+
+    #region Methods
+    //      -------
+
+    /// <summary>
+    /// If there is a new line at the end, it gets removed
+    /// </summary>
+    public static string RemoveNewLineAtEnd(string newString) {
+      if (newString.EndsWith(Environment.NewLine)) {
+        return newString.Substring(0, newString.Length-2);
+      } else {
+        return newString;
+      }
+    }
+
+
+    #endregion
+  }
+}
